2c17750a830c9e5653bd8e17d9b364a3da203e71
Tram <20522034@gm.uit.edu.vn>
2023-08-04 10:15:42 +0700

    create security

--- bookingTour/src/main/java/com/naite/bookingTour/security/WebSecurityConfig.java
+++ bookingTour/src/main/java/com/naite/bookingTour/security/WebSecurityConfig.java
@@ -1,32 +1,65 @@
-package com.naite.bookingTour;
+package com.naite.bookingTour.security;
 
 
-import java.util.List;
-
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        for (HttpMessageConverter<?> converter : converters) {
-            if (converter instanceof MappingJackson2HttpMessageConverter) {
-                MappingJackson2HttpMessageConverter jsonConverter = (MappingJackson2HttpMessageConverter) converter;
-                ObjectMapper objectMapper = jsonConverter.getObjectMapper();
-                objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-            }
-        }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) ->
+                        authorize
+                                .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
+//                                .requestMatchers("/cart/**").hasAnyAuthority("USER")
+                                .anyRequest().permitAll()
+                ).formLogin(
+                        form -> form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/login")
+                                .successHandler((request, response, authentication) -> {
+                                    if (authentication.getAuthorities().stream()
+                                            .anyMatch(auth -> auth.getAuthority().equals("ADMIN"))) {
+                                        response.sendRedirect("/admin");
+                                    } else {
+                                        response.sendRedirect("/");
+                                    }
+                                })
+                                .permitAll()
+                ).logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .permitAll()
+                ).exceptionHandling(
+                        exception -> exception
+                                .accessDeniedPage("/access-denied")
+                );
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
